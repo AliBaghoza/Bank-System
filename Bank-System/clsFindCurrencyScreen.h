114d539532e54995129a39288ca68ab7deaa94b1
Ali Salem <ali.s.baghoza@gmail.com>
2026-02-03 19:02:42 +0300

    Add Currency Exchange

---
+++ Bank-System/clsFindCurrencyScreen.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <iostream>
+#include "clsScreen.h"
+#include "clsCurrency.h"
+#include "clsInputValidate.h"
+
+class clsFindCurrencyScreen :protected clsScreen
+{
+
+private:
+    static void _PrintCurrency(clsCurrency Currency)
+    {
+        cout << "\nCurrency Card:\n";
+        cout << "_____________________________\n";
+        cout << "\nCountry    : " << Currency.Country();
+        cout << "\nCode       : " << Currency.CurrencyCode();
+        cout << "\nName       : " << Currency.CurrencyName();
+        cout << "\nRate(1$) = : " << Currency.Rate();
+
+        cout << "\n_____________________________\n";
+
+    }
+
+    static void _ShowResults(clsCurrency Currency)
+    {
+        if (!Currency.IsEmpty())
+        {
+            cout << "\nCurrency Found :-)\n";
+            _PrintCurrency(Currency);
+        }
+        else
+        {
+            cout << "\nCurrency Was not Found :-(\n";
+        }
+    }
+
+public:
+
+    static void ShowFindCurrencyScreen()
+    {
+
+        _DrawScreenHeader("\t  Find Currency Screen");
+
+      
+       string CurrencyCode;
+       cout << "\nPlease Enter CurrencyCode Or Country : ";
+       CurrencyCode = clsInputValidate::ReadString();
+       clsCurrency Currency = clsCurrency::FindByCodeOrCountry(CurrencyCode);
+       _ShowResults(Currency);
+        
+        
+    }
+
+};
+
